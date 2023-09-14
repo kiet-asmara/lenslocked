d1cf7f7965b9d5d61a0b2c8c52ea0b2885f0f27a
kiet <kiet123pascal@gmail.com>
2023-09-14 09:51:18 +0700

    first commit, add home template

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tplPath := filepath.Join("Templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("Parsing template %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Info</h1><p>Reach me at <a href=\"kiet123pascal@gmail.com\">kiet123pascal@gmail.com</a></p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+	<h1>FAQ Page</h1>
+	<ul>
+		<li><b>Is there a free version?</b> Yes there is bro</li>
+		<li><b>What are your support hours?</b> From 9AM-6PM</li>
+	</ul>
+	`)
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("start at :3000")
+	http.ListenAndServe(":3000", r)
+}
+
+// func main() {
+// 	handlerIndex := func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("hello"))
+// 	}
+
+// 	http.HandleFunc("/", handlerIndex)
+// 	address := "localhost:9000"
+// 	fmt.Println("start server at ", address)
+// 	err := http.ListenAndServe(address, nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// }
