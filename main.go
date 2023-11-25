21668d0d7aef2c3f6a52b2cf9e41408c78683709
kiet <kiet123pascal@gmail.com>
2023-11-25 10:47:56 +0700

    made simple chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +15,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>Get in touch with me! <a href=\"mailto:kiet123pascal@gmail.com\">kiet123pascal@gmail.com</a>,")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ</h1><p>Q: Is there a free version?</p><p>A: Nope</p></br><p>Q: How do I contact support?</p><p>A: Email us - <a href=\"mailto:kiet123pascal@gmail.com\">kiet123pascal@gmail.com</a>")
 }
 
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,25 +33,30 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
-// type Router struct{}
+type Router struct{}
 
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
 
 func main() {
-	// handlefunc = take function w/servehttp method -> for handler functions
-	// handle = take func of type http.handleRfunc -> for handlers
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting server on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 
 }
