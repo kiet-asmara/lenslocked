5d31e358e2e0a6cd6a2451ac5c29600fb352c8d7
kiet <kiet123pascal@gmail.com>
2023-12-19 09:12:18 +0700

    styled header

--- main.go
+++ main.go
@@ -10,41 +10,6 @@ import (
 	"github.com/kiet-asmara/lenslocked/views"
 )
 
-// func executeTemplate(w http.ResponseWriter, filepath string) {
-// 	t, err := views.Parse(filepath)
-// 	if err != nil {
-// 		log.Printf("parsing template: %v", err)
-// 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	t.Execute(w, nil)
-// }
-
-// func contactHandler(w http.ResponseWriter, r *http.Request) {
-// 	tplPath := filepath.Join("templates", "contact.gohtml")
-// 	executeTemplate(w, tplPath)
-// }
-
-// func faqHandler(w http.ResponseWriter, r *http.Request) {
-// 	tplPath := filepath.Join("templates", "faq.gohtml")
-// 	executeTemplate(w, tplPath)
-// }
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/faq":
-// 		faqHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
-
 func main() {
 	r := chi.NewRouter()
 
