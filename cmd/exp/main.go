d69ced7423c280fbef6900c538662df74996b882
kiet <kiet123pascal@gmail.com>
2024-01-12 21:33:36 +0700

    init edit

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,8 +7,30 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type DbConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg DbConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	cfg := DbConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
