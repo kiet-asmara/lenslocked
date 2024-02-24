8707fdb9f5640d4671a8a40c1e0e13c163ae29e3
kiet <kiet123pascal@gmail.com>
2024-02-24 14:59:37 +0700

    add cookie helpers in controllers

--- controllers/users.go
+++ controllers/users.go
@@ -45,15 +45,8 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/", // root: all pages
-		HttpOnly: true,
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
 	http.Redirect(w, r, "users/me", http.StatusFound)
-	fmt.Fprintf(w, "User created: %+v", user)
 }
 
 // sign in controller
@@ -86,14 +79,7 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// must be placed before anything is written to writer
-	cookie := http.Cookie{
-		Name:     "session",
-		Value:    session.Token,
-		Path:     "/",
-		HttpOnly: true, // to prevent JS script from accessing cookies console.log(document.cookie) to test
-	}
-	http.SetCookie(w, &cookie)
+	setCookie(w, CookieSession, session.Token)
 	http.Redirect(w, r, "/users/me", http.StatusFound)
 	fmt.Fprintf(w, "User authenticated %v", user)
 	// no view/template to render, only processing
@@ -101,13 +87,13 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 
 // take user info and print
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
-	token, err := r.Cookie("session")
+	token, err := readCookie(r, CookieSession)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
-	user, err := u.SessionService.User(token.Value)
+	user, err := u.SessionService.User(token)
 	if err != nil {
 		fmt.Println(err)
 		http.Redirect(w, r, "/signin", http.StatusFound)
